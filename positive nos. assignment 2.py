5b2b1d67589875289006d8fd430ffdfa7d636dfb
i-Aditi <66716360+i-Aditi@users.noreply.github.com>
2020-06-11 12:49:43 +0530

    Positive numbers assignment

---
+++ positive nos. assignment 2.py
@@ -0,0 +1,21 @@
+list1 =[]
+print("enter any 5 nos.")
+for i in range(5):
+    list1.append(int(input()))
+
+print("the nos. you have entered are: ",list1)
+
+
+for i in list1:
+    if i>=0:
+        print ("the positive numbers are: ",i)
+
+
+
+
+
+
+    
+    
+    
+    
